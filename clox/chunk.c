26ce74d4beb4c26562d989ee4e653a497b1d2e38
Tian Zhou <tianzhou93@gmail.com>
2022-08-05 10:40:59 -0700

    Fixed incorrect type

--- clox/chunk.c
+++ clox/chunk.c
@@ -23,7 +23,7 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(uint8_t, chunk->lines, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
