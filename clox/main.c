cbfd35b36c34a764aea5b19f67dda191fafc9eee
Tian Zhou <tianzhou93@gmail.com>
2022-08-05 00:44:06 -0700

    Added chunk printing

--- clox/main.c
+++ clox/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-#include "common.h"
 #include "chunk.h"
+#include "common.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
-    printf("Hello");
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
     return 0;
