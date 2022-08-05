a1ac87c671eb6a851fbb0ef31e76c7cd254f3e51
Tian Zhou <tianzhou93@gmail.com>
2022-08-05 13:42:23 -0700

    Added vm

---
+++ clox/vm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "vm.h"
+
+static InterpretResult run();
+
+void initVM(VM* vm) {
+
+}
+
+void freeVM(VM* vm) {
+
+}
+
+InterpretResult interpret(VM* vm, Chunk* chunk) {
+    vm->chunk = chunk;
+    vm->ip = vm->chunk->code;
+    return run();
+}
+
+static InterpretResult run(VM* vm) {
+#define READ_BYTE() (vm->ip++)
+#define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
+
+    for (;;) {
+        uint8_t instruction;
+        // instruction decoding / dispatching
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT:
+                Value constant = READ_CONSTANT();
+                printValue(constant);
+                printf("\n");
+                break;
+            case OP_RETURN:
+                return INTERPRET_OK;
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
