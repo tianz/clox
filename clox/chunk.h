7038ea2bc5c8440f719cc0019aeeac52b079af9c
Tian Zhou <tianzhou93@gmail.com>
2022-08-05 00:44:07 -0700

    Added line numbers

--- clox/chunk.h
+++ clox/chunk.h
@@ -13,12 +13,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines; // A parallel array that stores the line numbers
     ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
