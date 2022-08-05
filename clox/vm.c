6afe17b67fb6b88daf9f481d23fa2125a66e5926
Tian Zhou <tianzhou93@gmail.com>
2022-08-05 14:04:12 -0700

    Fixed errors

--- clox/vm.c
+++ clox/vm.c
@@ -16,24 +16,26 @@ void freeVM(VM* vm) {
 InterpretResult interpret(VM* vm, Chunk* chunk) {
     vm->chunk = chunk;
     vm->ip = vm->chunk->code;
-    return run();
+    return run(vm);
 }
 
 static InterpretResult run(VM* vm) {
-#define READ_BYTE() (vm->ip++)
+#define READ_BYTE() (*vm->ip++)
 #define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
 
     for (;;) {
         uint8_t instruction;
         // instruction decoding / dispatching
         switch (instruction = READ_BYTE()) {
-            case OP_CONSTANT:
+            case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 printValue(constant);
                 printf("\n");
                 break;
-            case OP_RETURN:
+            }
+            case OP_RETURN: {
                 return INTERPRET_OK;
+            }
         }
     }
 
