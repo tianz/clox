1f65adaf9796b8112c71ba10bb626ea7a5898153
Tian Zhou <tianzhou93@gmail.com>
2022-08-12 20:24:38 -0700

    Added synchronize

--- clox/compiler.c
+++ clox/compiler.c
@@ -36,6 +36,7 @@ static Chunk* currentChunk();
 static void error(const char* message);
 static void errorAtCurrent(const char* message);
 static void errorAt(Token* token, const char* message);
+static void synchronize();
 
 Scanner scanner;
 Parser parser;
@@ -356,4 +357,29 @@ static void errorAt(Token* token, const char* message) {
 
     fprintf(stderr, ": %s\n", message);
     parser.hadError = true;
+}
+
+static void synchronize() {
+    parser.panicMode = false;
+
+    while (parser.current.type != TOKEN_EOF) {
+        if (parser.previous.type == TOKEN_SEMICOLON) {
+            return;
+        }
+        switch (parser.current.type) {
+            case TOKEN_CLASS:
+            case TOKEN_FUN:
+            case TOKEN_VAR:
+            case TOKEN_FOR:
+            case TOKEN_IF:
+            case TOKEN_WHILE:
+            case TOKEN_PRINT:
+            case TOKEN_RETURN:
+                return;
+            default:
+                ; // do nothing
+        }
+
+        advance();
+    }
 }
